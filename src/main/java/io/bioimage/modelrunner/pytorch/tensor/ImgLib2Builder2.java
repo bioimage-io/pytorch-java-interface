e0984a1f7dca77f7057eef64a76951f9b8d1889d
carlosuc3m <100329787@alumnos.uc3m.es>
2023-10-16 15:24:59 +0200

    blueprint for optimized tensor conversion

--- src/main/java/io/bioimage/modelrunner/pytorch/tensor/ImgLib2Builder2.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/tensor/ImgLib2Builder2.java
@@ -24,21 +24,15 @@ package io.bioimage.modelrunner.pytorch.tensor;
 import io.bioimage.modelrunner.utils.IndexingUtils;
 
 import net.imglib2.Cursor;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.transform.integer.MixedTransform;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
-import net.imglib2.view.MixedTransformView;
-import net.imglib2.view.ViewTransforms;
-import net.imglib2.view.Views;
+
 import ai.djl.ndarray.NDArray;
 
 /**
@@ -48,7 +42,7 @@ import ai.djl.ndarray.NDArray;
 * 
 * @author Carlos Garcia Lopez de Haro
 */
-public class ImgLib2Builder {
+public class ImgLib2Builder2 {
 
 	/**
 	 * Creates a {@link Img} from a given {@link ai.djl.ndarray.NDArray} 
@@ -89,19 +83,24 @@ public class ImgLib2Builder {
 	 * 	The {@link NDArray} data is read from.
 	 * @return The {@link Img} built from the tensor of type {@link UnsignedByteType}.
 	 */
-	private static RandomAccessibleInterval<UnsignedByteType> buildFromTensorUByte(NDArray tensor) {
-		long[] arrayShape = tensor.getShape().getShape();
-		long[] tensorShape = new long[arrayShape.length];
-		for (int i = 0; i < arrayShape.length; i ++) tensorShape[i] = arrayShape[arrayShape.length - 1 - i];
+	private static Img<UnsignedByteType> buildFromTensorUByte(NDArray tensor) {
+		long[] tensorShape = tensor.getShape().getShape();
+		final ArrayImgFactory<UnsignedByteType> factory = new ArrayImgFactory<>(new UnsignedByteType());
+		final Img<UnsignedByteType> outputImg = factory.create(tensorShape);
+		Cursor<UnsignedByteType> tensorCursor = outputImg.cursor();
 		byte[] flatArr = tensor.toByteArray();
-		RandomAccessibleInterval<UnsignedByteType> rai = ArrayImgs.unsignedBytes(flatArr, tensorShape);
-		MixedTransform t = new MixedTransform( tensorShape.length, tensorShape.length );
-		int[] transposeAxesOrderChange = new int[tensorShape.length];
-		for (int i = 0; i < tensorShape.length; i ++) transposeAxesOrderChange[i] = tensorShape.length - 1 - i;
-		t.setComponentMapping(transposeAxesOrderChange);
-		long[] minMax = new long[tensorShape.length];
-		for (int i = 0; i < tensorShape.length; i ++) minMax[i * 2 + 1] = tensorShape[i];
-		return Views.interval(new MixedTransformView<UnsignedByteType>( rai, t ), Intervals.createMinMax(tensorShape));
+		while (tensorCursor.hasNext()) {
+			tensorCursor.fwd();
+			long[] cursorPos = tensorCursor.positionAsLongArray();
+			int flatPos = IndexingUtils.multidimensionalIntoFlatIndex(cursorPos,
+				tensorShape);
+			byte val = flatArr[flatPos];
+			if (val < 0)
+				tensorCursor.get().set(256 + (int) val);
+			else
+				tensorCursor.get().set(val);
+		}
+		return outputImg;
 	}
 
 	/**
