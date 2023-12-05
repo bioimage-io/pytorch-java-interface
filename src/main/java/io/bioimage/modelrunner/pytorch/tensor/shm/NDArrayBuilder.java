adfa4eef42959387191616be0918e7fb1337d2aa
carlosuc3m <100329787@alumnos.uc3m.es>
2023-12-05 19:29:49 +0100

    copy directly from shm into tensor

--- src/main/java/io/bioimage/modelrunner/pytorch/tensor/shm/NDArrayBuilder.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/tensor/shm/NDArrayBuilder.java
@@ -21,6 +21,7 @@
 
 package io.bioimage.modelrunner.pytorch.tensor.shm;
 
+import io.bioimage.modelrunner.numpy.DecodeNumpy;
 import io.bioimage.modelrunner.tensor.Tensor;
 import io.bioimage.modelrunner.tensor.Utils;
 import io.bioimage.modelrunner.utils.CommonUtils;
@@ -28,7 +29,9 @@ import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.blocks.PrimitiveBlocks;
 import net.imglib2.img.Img;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.Type;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.real.DoubleType;
@@ -36,7 +39,15 @@ import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.regex.Matcher;
 
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
@@ -264,4 +275,119 @@ public class NDArrayBuilder {
 		NDArray ndarray = manager.create(flatArr, new Shape(ogShape));
 		return ndarray;
 	}
+    
+    /**
+     * MEthod to decode the bytes corresponding to a numpy array stored in the numpy file
+     * @param <T>
+     * 	possible data types that the ImgLib2 image can have
+     * @param is
+     * 	{@link InputStream} that results after reading the numpy file. Contains the byte info of the
+     * 	numpy array
+     * @return an ImgLib2 image with the same datatype, shape and data that the numpy array
+     * @throws IOException if there is any error reading the {@link InputStream}
+     */
+    public static < T extends RealType< T > & NativeType< T > > 
+    				HashMap<String, Object> decodeNumpyFromByteArrayStream(InputStream is) throws IOException {
+        DataInputStream dis;
+        if (is instanceof DataInputStream) {
+            dis = (DataInputStream) is;
+        } else {
+            dis = new DataInputStream(is);
+        }
+
+        byte[] buf = new byte[DecodeNumpy.MAGIC_PREFIX.length];
+        dis.readFully(buf);
+        if (!Arrays.equals(buf, DecodeNumpy.MAGIC_PREFIX)) {
+            throw new IllegalArgumentException("Malformed  or unsopported Numpy array");
+        }
+        byte major = dis.readByte();
+        byte minor = dis.readByte();
+        if (major < 1 || major > 3 || minor != 0) {
+            throw new IllegalArgumentException("Unknown numpy version: " + major + '.' + minor);
+        }
+        int len = major == 1 ? 2 : 4;
+        dis.readFully(buf, 0, len);
+        ByteBuffer bb = ByteBuffer.wrap(buf, 0, len);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        if (major == 1) {
+            len = bb.getShort();
+        } else {
+            len = bb.getInt();
+        }
+        buf = new byte[len];
+        dis.readFully(buf);
+        String header = new String(buf, StandardCharsets.UTF_8);
+        Matcher m = DecodeNumpy.HEADER_PATTERN.matcher(header);
+        if (!m.find()) {
+            throw new IllegalArgumentException("Invalid numpy header: " + header);
+        }
+        String typeStr = m.group(1);
+        String fortranOrder = m.group(2).trim();
+        String shapeStr = m.group(3);
+        long[] shape = new long[0];
+        if (!shapeStr.isEmpty()) {
+            String[] tokens = shapeStr.split(", ?");
+            shape = Arrays.stream(tokens).mapToLong(Long::parseLong).toArray();
+        }
+        char order = typeStr.charAt(0);
+        ByteOrder byteOrder = null;
+        if (order == '>') {
+        	byteOrder = ByteOrder.BIG_ENDIAN;
+        } else if (order == '<') {
+        	byteOrder = ByteOrder.LITTLE_ENDIAN;
+        } else if (order == '|') {
+        	byteOrder = ByteOrder.LITTLE_ENDIAN;
+        	new IOException("Numpy .npy file did not specify the byte order of the array."
+        			+ " It was automatically opened as little endian but this does not guarantee"
+        			+ " the that the file is open correctly. Caution is advised.").printStackTrace();
+    	} else {
+        	new IllegalArgumentException("Not supported ByteOrder for the provided .npy array.");
+        }
+        String dtype = DecodeNumpy.getDataType(typeStr.substring(1));
+        long numBytes = DecodeNumpy.DATA_TYPES_MAP.get(dtype);
+    	long count;
+    	if (shape.length == 0)
+    		count = 1;
+		else
+			count = Arrays.stream(shape).reduce(Math::multiplyExact).getAsLong();
+        //len = Math.toIntExact(shape.length * numBytes);
+        len = Math.toIntExact(count * numBytes);
+        ByteBuffer data = ByteBuffer.allocate(len);
+        data.order(byteOrder);
+        readData(dis, data, len);
+        
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("shape", shape);
+        map.put("byte_order", byteOrder);
+        map.put("dtype", dtype);
+        map.put("fortran_order", fortranOrder.equals("True"));
+        map.put("data", data);
+
+        return map;
+    }
+
+    /**
+     * Read the data from the input stream into a byte buffer
+     * @param dis
+     * 	the {@link DataInputStream} from where the data is read from
+     * @param data
+     * 	{@link ByteBuffer} where the info is copied to
+     * @param len
+     * 	remaining number of bytes in the {@link DataInputStream}
+     * @throws IOException if there is any error reading the {@link DataInputStream}
+     */
+    private static void readData(DataInputStream dis, ByteBuffer data, int len) throws IOException {
+        if (len > 0) {
+            byte[] buf = new byte[DecodeNumpy.BUFFER_SIZE];
+            while (len > DecodeNumpy.BUFFER_SIZE) {
+                dis.readFully(buf);
+                data.put(buf);
+                len -= DecodeNumpy.BUFFER_SIZE;
+            }
+
+            dis.readFully(buf, 0, len);
+            data.put(buf, 0, len);
+            data.rewind();
+        }
+    }
 }
