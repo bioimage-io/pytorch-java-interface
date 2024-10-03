154fb2d6dbe3319a2c33e6fb171786a78c0e873c
carlosuc3m <100329787@alumnos.uc3m.es>
2024-10-03 19:07:52 +0200

    correct paths for win

--- src/main/java/io/bioimage/modelrunner/pytorch/PytorchInterface.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/PytorchInterface.java
@@ -311,8 +311,8 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 	void runInterprocessing(List<Tensor<T>> inputTensors, List<Tensor<R>> outputTensors) throws RunModelException {
 		shmaInputList = new ArrayList<SharedMemoryArray>();
 		shmaOutputList = new ArrayList<SharedMemoryArray>();
-		List<String> encIns = modifyForWinCmd(encodeInputs(inputTensors));
-		List<String> encOuts = modifyForWinCmd(encodeOutputs(outputTensors));
+		List<String> encIns = encodeInputs(inputTensors);
+		List<String> encOuts = encodeOutputs(outputTensors);
 		LinkedHashMap<String, Object> args = new LinkedHashMap<String, Object>();
 		args.put("inputs", encIns);
 		args.put("outputs", encOuts);
@@ -383,15 +383,6 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			try { shm.close(); } catch (IOException e1) { e1.printStackTrace();}
 		});
 		shmaOutputList = null;
-	}
-	
-	private static List<String> modifyForWinCmd(List<String> ins){
-		if (!PlatformDetection.isWindows())
-			return ins;
-		List<String> newIns = new ArrayList<String>();
-		for (String ii : ins)
-			newIns.add("\"" + ii.replace("\"", "\\\"") + "\"");
-		return newIns;
 	}
 	
 	
