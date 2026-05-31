f953676338cb2c360645f769860bcf4e44cd7a32
carlosuc3m <100329787@alumnos.uc3m.es>
2026-05-31 04:33:05 +0200

    adapt

--- src/main/java/io/bioimage/modelrunner/pytorch/PytorchInterface.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/PytorchInterface.java
@@ -20,10 +20,6 @@
  */
 package io.bioimage.modelrunner.pytorch;
 
-import io.bioimage.modelrunner.apposed.appose.Service;
-import io.bioimage.modelrunner.apposed.appose.Types;
-import io.bioimage.modelrunner.apposed.appose.Service.Task;
-import io.bioimage.modelrunner.apposed.appose.Service.TaskStatus;
 import io.bioimage.modelrunner.engine.DeepLearningEngineInterface;
 import io.bioimage.modelrunner.exceptions.LoadModelException;
 import io.bioimage.modelrunner.exceptions.RunModelException;
@@ -59,6 +55,12 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.apposed.appose.Service;
+import org.apposed.appose.Service.Task;
+import org.apposed.appose.Service.TaskStatus;
+import org.apposed.appose.TaskException;
+import org.apposed.appose.util.Messages;
 
 import com.google.gson.Gson;
 
@@ -175,8 +177,8 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 		if (interprocessing) {
 			try {
 				launchModelLoadOnProcess();
-			} catch (IOException | InterruptedException e) {
-				throw new LoadModelException(Types.stackTrace(e));
+			} catch (IOException | InterruptedException | TaskException e) {
+				throw new LoadModelException(Messages.stackTrace(e));
 			}
 			return;
 		}
@@ -200,11 +202,11 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 		catch (Exception e) {
 			e.printStackTrace();
 			Utils.managePytorchExceptions(e);
-			throw new LoadModelException("Error loading a Pytorch model", Types.stackTrace(e));
+			throw new LoadModelException("Error loading a Pytorch model", Messages.stackTrace(e));
 		}
 	}
 	
-	private void launchModelLoadOnProcess() throws IOException, InterruptedException {
+	private void launchModelLoadOnProcess() throws IOException, InterruptedException, TaskException {
 		HashMap<String, Object> args = new HashMap<String, Object>();
 		args.put("modelFolder", modelFolder);
 		args.put("modelSource", modelSource);
@@ -268,7 +270,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			return outs;
 		}
 		catch (TranslateException e) {
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 
@@ -305,7 +307,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			fillOutputTensors(outputNDArrays, outputTensors);
 		}
 		catch (TranslateException e) {
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 	
@@ -315,7 +317,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			// corresponding names
 			NDList inputList = new NDList();
 			for (String ee : inputs) {
-				Map<String, Object> decoded = Types.decode(ee);
+				Map<String, Object> decoded = Messages.decode(ee);
 				SharedMemoryArray shma = SharedMemoryArray.read((String) decoded.get(MEM_NAME_KEY));
 				NDArray inT = TensorBuilder.build(shma, manager);
 				if (PlatformDetection.isWindows()) shma.close();
@@ -327,12 +329,12 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 
 			int c = 0;
 			for (String ee : outputs) {
-				Map<String, Object> decoded = Types.decode(ee);
+				Map<String, Object> decoded = Messages.decode(ee);
 				ShmBuilder.build(outputNDArrays.get(c ++), (String) decoded.get(MEM_NAME_KEY));
 			}
 		}
 		catch (TranslateException e) {
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 	
@@ -342,7 +344,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			// corresponding names
 			NDList inputList = new NDList();
 			for (String ee : inputs) {
-				Map<String, Object> decoded = Types.decode(ee);
+				Map<String, Object> decoded = Messages.decode(ee);
 				SharedMemoryArray shma = SharedMemoryArray.read((String) decoded.get(MEM_NAME_KEY));
 				NDArray inT = TensorBuilder.build(shma, manager);
 				if (PlatformDetection.isWindows()) shma.close();
@@ -361,7 +363,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			return shmaNamesList;
 		}
 		catch (TranslateException e) {
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 	
@@ -388,7 +390,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 				throw new RuntimeException(task.error);
 			}
 			for (int i = 0; i < outputTensors.size(); i ++) {
-	        	String name = (String) Types.decode(encOuts.get(i)).get(MEM_NAME_KEY);
+	        	String name = (String) Messages.decode(encOuts.get(i)).get(MEM_NAME_KEY);
 	        	SharedMemoryArray shm = shmaOutputList.stream()
 	        			.filter(ss -> ss.getName().equals(name)).findFirst().orElse(null);
 	        	if (shm == null) {
@@ -402,7 +404,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			closeShmas();
 			if (e instanceof RunModelException)
 				throw (RunModelException) e;
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 		closeShmas();
 	}
@@ -441,7 +443,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			List<String> outputs = (List<String>) task.outputs.get("encoded");
 			List<RandomAccessibleInterval<R>> rais = new ArrayList<RandomAccessibleInterval<R>>();
 			for (String out : outputs) {
-	        	String name = (String) Types.decode(out).get(MEM_NAME_KEY);
+	        	String name = (String) Messages.decode(out).get(MEM_NAME_KEY);
 	        	SharedMemoryArray shm = SharedMemoryArray.read(name);
 	        	RandomAccessibleInterval<R> rai = shm.getSharedRAI();
 	        	rais.add(Tensor.createCopyOfRaiInWantedDataType(Cast.unchecked(rai), Util.getTypeFromInterval(Cast.unchecked(rai))));
@@ -453,7 +455,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			closeShmas();
 			if (e instanceof RunModelException)
 				throw (RunModelException) e;
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 	
@@ -473,7 +475,7 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 		} catch (Exception e) {
 			if (e instanceof RunModelException)
 				throw (RunModelException) e;
-			throw new RunModelException(Types.stackTrace(e));
+			throw new RunModelException(Messages.stackTrace(e));
 		}
 	}
 
@@ -506,21 +508,17 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 		if (!PlatformDetection.isWindows())
 			return;
 		this.shmaNamesList.stream().forEach(nn -> {
-			try {
-				SharedMemoryArray.read(nn).close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			SharedMemoryArray.read(nn).close();
 		});
 	}
 	
 	private void closeShmas() throws RunModelException {
 		shmaInputList.forEach(shm -> {
-			try { shm.close(); } catch (IOException e1) { e1.printStackTrace();}
+			shm.close();
 		});
 		shmaInputList = null;
 		shmaOutputList.forEach(shm -> {
-			try { shm.close(); } catch (IOException e1) { e1.printStackTrace();}
+			shm.close();
 		});
 		shmaOutputList = null;
 		if (interprocessing)
@@ -587,8 +585,8 @@ public class PytorchInterface implements DeepLearningEngineInterface {
 			try {
 				task = runner.task("close");
 				task.waitFor();
-			} catch (IOException | InterruptedException e) {
-				throw new RuntimeException(Types.stackTrace(e));
+			} catch (TaskException | InterruptedException e) {
+				throw new RuntimeException(Messages.stackTrace(e));
 			}
 			if (task.status == TaskStatus.CANCELED)
 				throw new RuntimeException();
